b5e632e4976e0e0fcec87407491444fa43e51d0f
Laerson Castro <laersonjr@gmail.com>
2022-06-30 20:59:09 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/laerson/trace/finance/paymentapi/ProjectJavaWithSpringPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.laerson.trace.finance.paymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaWithSpringPaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJavaWithSpringPaymentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laerson/trace/finance/paymentapi/ProjectJavaWithSpringPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerson.trace.finance.paymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaWithSpringPaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
