9fccb0c840ca6dccf23f3835c7b269800f0a8c15
Laerson Castro <laersonjr@gmail.com>
2022-06-30 23:27:16 -0300

    :recycle:refactor Ajustando metodo de serviço para criar wallet

--- src/main/java/com/laerson/trace/finance/paymentapi/domain/service/WalletService.java
+++ src/main/java/com/laerson/trace/finance/paymentapi/domain/service/WalletService.java
@@ -2,6 +2,7 @@ package com.laerson.trace.finance.paymentapi.domain.service;
 
 import com.laerson.trace.finance.paymentapi.api.model.CreateWalletModel;
 import com.laerson.trace.finance.paymentapi.domain.model.Wallet;
+import com.laerson.trace.finance.paymentapi.domain.repository.WalletRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,12 +16,15 @@ public class WalletService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private WalletRepository walletRepository;
+
     public Wallet createWalletService(CreateWalletModel createWalletModel){
         Wallet wallet = toEntity(createWalletModel);
         wallet.setWalletName(wallet.getOwnerName());
         wallet.setValue(BigDecimal.ZERO);
         wallet.setWalletOpening(LocalDateTime.now());
-        return wallet;
+        return walletRepository.save(wallet);
     }
 
     private Wallet toEntity(CreateWalletModel createWalletModel){
