d425e0396c15774de3c53f5a45b737f8406813e6
Laerson Castro <laersonjr@gmail.com>
2022-06-30 22:39:02 -0300

    :heavy_plus_sign:build Endpoint para criar um recurso de wallet

--- src/main/java/com/laerson/trace/finance/paymentapi/api/controller/WalletController.java
+++ src/main/java/com/laerson/trace/finance/paymentapi/api/controller/WalletController.java
@@ -3,10 +3,11 @@ package com.laerson.trace.finance.paymentapi.api.controller;
 import com.laerson.trace.finance.paymentapi.domain.model.Wallet;
 import com.laerson.trace.finance.paymentapi.domain.repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,12 @@ public class WalletController {
     @GetMapping
     public List<Wallet> listarWallet(){
         return walletRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Wallet> criarWallet(@Valid @RequestBody Wallet wallet){
+        Wallet walletSalva = walletRepository.save(wallet);
+        return ResponseEntity.status(HttpStatus.CREATED).body(walletSalva);
     }
 
 }
