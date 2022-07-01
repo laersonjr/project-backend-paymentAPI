f42c641b68fc31f24ab080773e44e12b092b619a
Laerson Castro <laersonjr@gmail.com>
2022-06-30 22:11:35 -0300

    :sparkles: feat: Criado controller de wallets

---
+++ src/main/java/com/laerson/trace/finance/paymentapi/api/controller/WalletController.java
@@ -0,0 +1,21 @@
+package com.laerson.trace.finance.paymentapi.api.controller;
+
+import com.laerson.trace.finance.paymentapi.domain.model.Wallet;
+import com.laerson.trace.finance.paymentapi.domain.repository.WalletRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/wallets")
+public class WalletController {
+
+    @Autowired
+    private WalletRepository walletRepository;
+
+
+
+}
