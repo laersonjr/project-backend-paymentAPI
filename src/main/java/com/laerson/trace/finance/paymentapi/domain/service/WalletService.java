3227e393944515d691c33e8f1f4ad92ce5d33e19
Laerson Castro <laersonjr@gmail.com>
2022-06-30 23:16:51 -0300

    :dizzy: Criado metodo para criar wallet recebendo um createWalletModel

--- src/main/java/com/laerson/trace/finance/paymentapi/domain/service/WalletService.java
+++ src/main/java/com/laerson/trace/finance/paymentapi/domain/service/WalletService.java
@@ -6,11 +6,22 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Service
 public class WalletService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    public Wallet createWalletService(CreateWalletModel createWalletModel){
+        Wallet wallet = toEntity(createWalletModel);
+        wallet.setWalletName(wallet.getOwnerName());
+        wallet.setValue(BigDecimal.ZERO);
+        wallet.setWalletOpening(LocalDateTime.now());
+        return wallet;
+    }
 
     private Wallet toEntity(CreateWalletModel createWalletModel){
         return modelMapper.map(createWalletModel, Wallet.class);
