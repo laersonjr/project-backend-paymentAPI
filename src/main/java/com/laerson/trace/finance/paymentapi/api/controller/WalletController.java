1443556ba16f908a5383b7ca86a937752a6265bf
Laerson Castro <laersonjr@gmail.com>
2022-07-01 00:02:11 -0300

    :construction: Ajustando Recurso para criar Wallet

--- src/main/java/com/laerson/trace/finance/paymentapi/api/controller/WalletController.java
+++ src/main/java/com/laerson/trace/finance/paymentapi/api/controller/WalletController.java
@@ -1,7 +1,9 @@
 package com.laerson.trace.finance.paymentapi.api.controller;
 
+import com.laerson.trace.finance.paymentapi.api.model.CreateWalletModel;
 import com.laerson.trace.finance.paymentapi.domain.model.Wallet;
 import com.laerson.trace.finance.paymentapi.domain.repository.WalletRepository;
+import com.laerson.trace.finance.paymentapi.domain.service.WalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,14 +19,17 @@ public class WalletController {
     @Autowired
     private WalletRepository walletRepository;
 
+    @Autowired
+    private WalletService walletService;
+
     @GetMapping
     public List<Wallet> listarWallet(){
         return walletRepository.findAll();
     }
 
     @PostMapping
-    public ResponseEntity<Wallet> criarWallet(@Valid @RequestBody Wallet wallet){
-        Wallet walletSalva = walletRepository.save(wallet);
+    public ResponseEntity<Wallet> criarWallet(@Valid @RequestBody CreateWalletModel createWalletModel){
+        Wallet walletSalva = walletService.createWalletService(createWalletModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(walletSalva);
     }
 
