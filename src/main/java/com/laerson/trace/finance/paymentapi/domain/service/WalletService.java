4c930534a10ef16254f65c0925e6412d0b328265
Laerson Castro <laersonjr@gmail.com>
2022-06-30 23:09:08 -0300

    :sparkles:feat Criado classe de serviço para Wallet

---
+++ src/main/java/com/laerson/trace/finance/paymentapi/domain/service/WalletService.java
@@ -0,0 +1,14 @@
+package com.laerson.trace.finance.paymentapi.domain.service;
+
+import com.laerson.trace.finance.paymentapi.api.model.CreateWalletModel;
+import com.laerson.trace.finance.paymentapi.domain.model.Wallet;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WalletService {
+
+
+
+}
