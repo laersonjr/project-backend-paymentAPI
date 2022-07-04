ad6532b95447178ae86b0f6fbf18c0fbe25404bc
Laerson Castro <laersonjr@gmail.com>
2022-07-03 23:07:30 -0300

    :ok_hand:styly Criando padrão para o com dDate aceitar apenas campos no formato yyyy-mm-dd

--- src/main/java/com/laerson/trace/finance/paymentapi/api/model/PaymentWalletModel.java
+++ src/main/java/com/laerson/trace/finance/paymentapi/api/model/PaymentWalletModel.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 @Getter
@@ -14,6 +15,7 @@ public class PaymentWalletModel {
     private BigDecimal amount;
 
     @NotNull
+    @Pattern(regexp = "^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$")
     private String date;
 
 }
